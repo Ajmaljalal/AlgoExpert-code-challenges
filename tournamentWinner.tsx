8fe40f3da6862c6e1bfac67e03986179b50584e2
Ajmal Jalal <ajmaljalal58@gmail.com>
2021-05-10 20:03:17 -0700

    TournamentWinner solution 2

--- tournamentWinner.tsx
+++ tournamentWinner.tsx
@@ -1,6 +1,6 @@
 // Solution One
 // O(n^2) time | O(n^2) space
-export function tournamentWinner(competitions: string[][], results: number[]) {
+const tournamentWinner1 = (competitions: string[][], results: number[]) => {
   const winners: any = {}
   const teams: any = []
 
@@ -24,4 +24,28 @@ export function tournamentWinner(competitions: string[][], results: number[]) {
     }
   })
   return winner
+}
+
+// Solution two
+// O(n) time | O(k) space
+const tournamentWinner2 = (competitions: string[][], results: number[]) => {
+  const winners: any = {}
+  let currentRoundWinner: string = ''
+  winners[currentRoundWinner] = 0
+  results.forEach((res: number, i: number) => {
+    if (res === 0) {
+      const winnerScore = winners[competitions[i][1]] ? winners[competitions[i][1]] + 3 : 3
+      winners[competitions[i][1]] = winnerScore
+      if (winners[competitions[i][1]] > winners[currentRoundWinner]) {
+        currentRoundWinner = competitions[i][1]
+      }
+    } else {
+      const winnerScore = winners[competitions[i][0]] ? winners[competitions[i][0]] + 3 : 3
+      winners[competitions[i][0]] = winnerScore
+      if (winners[competitions[i][0]] > winners[currentRoundWinner]) {
+        currentRoundWinner = competitions[i][0]
+      }
+    }
+  })
+  return currentRoundWinner
 }
