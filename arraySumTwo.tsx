77360ef437be29352a422dbed05483309231442d
Ajmal Jalal <ajmaljalal58@gmail.com>
2021-09-03 19:42:17 -0700

    Array-Sum-Two solution 3

--- arraySumTwo.tsx
+++ arraySumTwo.tsx
@@ -20,15 +20,40 @@ const twoNumberSum1 = (array: number[], targetSum: number) => {
 // Solution Two
 // O(n) time | O(n) space
 const twoNumberSum2 = (arr: number[], targetSum: number) => {
-    const numbersCheck: {[key:number]: boolean} = {}
+    const numbersCheck: { [key: number]: boolean } = {}
     for (const num of arr) {
+        let secondNumber
+        let firstNumber
         const potentialMatch = targetSum - num;
-        if (potentialMatch  in numbersCheck) {
-            return [potentialMatch, num];
-        }else {
+        if (potentialMatch in numbersCheck) {
+            firstNumber = potentialMatch
+            secondNumber = num
+            return [firstNumber, secondNumber];
+        } else {
             numbersCheck[num] = true;
         }
     }
+    return []
+}
+
+// Solution Three
+// O(nlog(n)) time | O(1) space
+const twoNumberSum3 = (arr: number[], targetSum: number) => {
+    arr.sort((a,b)=> a-b)
+    let leftIndex = 0;
+    let rightIndex = arr.length - 1;
+
+    while (leftIndex < rightIndex) {
+        const sumOfTwoIndexValues = arr[leftIndex] + arr[rightIndex];
+        if (sumOfTwoIndexValues === targetSum) {
+            return [arr[leftIndex], arr[rightIndex]];
+        } else if (sumOfTwoIndexValues < targetSum) {
+            leftIndex++;
+        } else if (sumOfTwoIndexValues > targetSum) {
+            rightIndex--;
+        }
+    }
+    return []
 }
 
 
