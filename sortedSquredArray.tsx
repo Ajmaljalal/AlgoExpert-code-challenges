d76acc836c7d6e2beb0aa96cf9fae21ce1f61e85
Ajmal Jalal <ajmaljalal58@gmail.com>
2021-09-09 19:54:06 -0700

    Code clean up and formating

--- sortedSquredArray.tsx
+++ sortedSquredArray.tsx
@@ -1,31 +1,31 @@
 // Solution One
 // O(nlogn) time | O(n) space
 const sortedSquaredArray1 = (array: number[]) => {
-    return array.map(number => {
-        return (number) * (number)
-    })
-        .sort((a, b) => a - b)
+  return array.map(number => {
+    return (number) * (number)
+  })
+    .sort((a, b) => a - b)
 }
 
 // Solution Two
 // O(n) time | O(n)
-const  sortedSquaredArray2 = (array: number[]) => {
-    let startIndex: number = 0;
-    let endIndex: number = array.length - 1
-    let sorteArrayIndex: number = array.length - 1
-    const sortedSquareArr: number[] = []
-    while (endIndex >= startIndex) {
-        const startAbsNum = Math.abs(array[startIndex])
-        const endAbsNum = Math.abs(array[endIndex])
-        if (startAbsNum > endAbsNum) {
-            sortedSquareArr[sorteArrayIndex] = startAbsNum * startAbsNum
-            startIndex += 1
-        }
-        if (startAbsNum <= endAbsNum) {
-            sortedSquareArr[sorteArrayIndex] = endAbsNum * endAbsNum
-            endIndex -= 1
-        }
-        sorteArrayIndex--
+const sortedSquaredArray2 = (array: number[]) => {
+  let startIndex: number = 0;
+  let endIndex: number = array.length - 1
+  let sorteArrayIndex: number = array.length - 1
+  const sortedSquareArr: number[] = []
+  while (endIndex >= startIndex) {
+    const startAbsNum = Math.abs(array[startIndex])
+    const endAbsNum = Math.abs(array[endIndex])
+    if (startAbsNum > endAbsNum) {
+      sortedSquareArr[sorteArrayIndex] = startAbsNum * startAbsNum
+      startIndex += 1
     }
-    return sortedSquareArr
+    if (startAbsNum <= endAbsNum) {
+      sortedSquareArr[sorteArrayIndex] = endAbsNum * endAbsNum
+      endIndex -= 1
+    }
+    sorteArrayIndex--
+  }
+  return sortedSquareArr
 }
